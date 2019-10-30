7d4e88e765e4e4c86573ddc2a5f14c4a96b16c4f
Piotr Sałkowski <ps@mosqi.to>
2019-10-30 12:40:48 +0100

    Add some styles. Add search bar

--- projects/gt-file-manager/src/lib/gt-file-manager.module.ts
+++ projects/gt-file-manager/src/lib/gt-file-manager.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {GtFileManagerComponent} from './gt-file-manager.component';
 import {HttpClientModule} from '@angular/common/http';
+import {GtFileResolveService} from './gt-file-resolve.service';
 
 
 
@@ -10,6 +11,9 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     CommonModule,
     HttpClientModule,
+  ],
+  providers: [
+    GtFileResolveService
   ],
   exports: [GtFileManagerComponent]
 })
--- projects/gt-file-manager/src/lib/gt-file-resolve.service.ts
+++ projects/gt-file-manager/src/lib/gt-file-resolve.service.ts
@@ -1,13 +1,57 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {of} from "rxjs";
+import {map, toArray} from "rxjs/operators";
 
 @Injectable()
 export class GtFileResolveService {
+  readonly baseUrl: string = 'http://localhost:3000/';
+
   constructor(private http: HttpClient) {
 
   }
 
-  public getFiles(param: any = null) {
-    this.http.get('http://localhost:3000/');
+  public files(path: any) {
+    const url: string = this.baseUrl + '/file/' + path;
+
+    return this.http.get(url);
+  }
+
+  public dummySearch(query: string) {
+    return of(...new Array<number>(10))
+      .pipe(
+        map(i => `https://picsum.photos/seed/${Math.random()}/500/400`),
+        toArray()
+      );
+  }
+
+  public search(query: string) {
+    let url: string = this.baseUrl + '/search';
+
+    if (query) {
+      url += '/' + query;
+    }
+
+    return this.http.get(url);
+  }
+
+  public upload(file, path: string = null) {
+    let url: string = this.baseUrl + '/upload';
+
+    if (path) {
+      url += '/' + path;
+    }
+
+    return this.http.post(url, file);
+  }
+
+  public list(path: string = null) {
+    let url: string = this.baseUrl;
+
+    if (path) {
+      url += path;
+    }
+
+    return this.http.get(url);
   }
 }
